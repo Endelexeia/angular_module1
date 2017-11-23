699d171aeaba41e1668362092b437b7953cfd699
Afionis <ndikaros@gmail.com>
2017-11-23 20:59:08 +0200

    23-11-2017_morning

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
   (function () {
     'use strict';
 
-  angular.module('lunch_checker',[])
-  .controller('main_controller', function($scope) {
-    
+  angular.module('LunchCheck',[])
+  .controller('LunchCheckController', function($scope) {
+
+
+  var size =   function splitString(stringToSplit, separator) {
+      var arrayOfStrings = stringToSplit.split(separator);
+
+      console.log('The original string is: "' + stringToSplit + '"');
+      console.log('The separator is: "' + separator + '"');
+      console.log('The array has ' + arrayOfStrings.length + ' elements: ' + arrayOfStrings.join(' / '));
+      return arrayOfStrings.length;
+  }
+
+
   });
 
   })();
