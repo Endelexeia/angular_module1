8a9531bce98826f4e85d8c3326c3fbb38d89ed53
Afionis <ndikaros@gmail.com>
2017-11-24 23:08:03 +0200

    final version

--- app.js
+++ app.js
@@ -8,19 +8,22 @@
     function LunchCheckController($scope,$filter){
       $scope.message;
       $scope.listOfFoods = "";
-      var separator = ",";
+      $scope.color = "";
 
       $scope.displayMessage = function(){
         var finalmessage = "";
-        var numOfArguments = splitString($scope.listOfFoods,separator)
+        var numOfArguments = splitString($scope.listOfFoods,",")
         if ( numOfArguments > 3  ) {
           finalmessage = "too many";
+          $scope.color = "green";
         }
         else if (numOfArguments <= 3 && numOfArguments > 0) {
           finalmessage = "enjoy";
+          $scope.color = "green";
         }
         else {
           finalmessage = "not enough arguments";
+          $scope.color = "red";
         }
         $scope.message = finalmessage;
 
@@ -29,8 +32,21 @@
         if (arrayOfStrings.length == 1 && arrayOfStrings[0].length == 0 )
           return 0;
         else
-          return arrayOfStrings.length;
+          return arrayOfStrings.length - findEmptyFields(arrayOfStrings);
       };
+
+      function findEmptyFields(array)
+      {
+        var counter = 0;
+        for (var i = 0; i < array.length; i++) {
+          if (array[i].trim() == "") {
+            counter++;
+          }
+
+        }
+        return counter;
+      }
+
     }
   }
 })();
